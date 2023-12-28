f9a8c758ff9b5874ea199101600ca13cd181832f
Swayam52 <swayamkothekar52@gmail.com>
2023-12-28 13:24:28 +0530

    added lap functions

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ let sec = 0;
 let min = 0;
 let hr = 0;
 let timer = null;
+let lapTimes = [];
 
-let displayTime = document.getElementById("displayTime")
+let displayTime = document.getElementById("displayTime");
+let lapList = document.getElementById("lapList");
+
+document.getElementById("pause").style.display = "none";
+document.getElementById("reset").style.display = "none";
+document.getElementById("lap").style.display = "none";
 
 const stopwatch = ()=>{
   sec++;
@@ -27,17 +33,48 @@ const startTimer = ()=>{
   if(timer !== null){
     clearInterval(timer);
   }
+  document.getElementById("start").style.display = "none";
+  document.getElementById("pause").style.display = "block";
+  document.getElementById("reset").style.display = "block";
+  document.getElementById("lap").style.display = "block";
   timer = setInterval(stopwatch, 1000);
 }
 
-const stopTimer = ()=>{
+const pauseTimer = ()=>{
+  document.getElementById("start").style.display = "block";
+  document.getElementById("pause").style.display = "none";
   clearInterval(timer);
 }
 
 const resetTimer = ()=>{
+  document.getElementById("start").style.display = "block";
+  document.getElementById("pause").style.display = "none";
+  document.getElementById("reset").style.display = "none";
+  document.getElementById("lap").style.display = "none";
   clearInterval(timer);
   sec = 0;
   min = 0;
   hr = 0;
+  lapTimes = [];
   displayTime.innerHTML = "00:00:00";
+  updateLapList();
 }
+
+const addLap = () => {
+  let lap_h = hr < 10 ? "0"+hr : hr;
+  let lap_m = min < 10 ? "0"+min : min;
+  let lap_s = sec < 10 ? "0"+sec : sec;
+
+  const lapTime = lap_h + ":" + lap_m + ":" + lap_s;
+  lapTimes.push(lapTime);
+  updateLapList();
+};
+
+const updateLapList = () => {
+  lapList.innerHTML = "";
+  lapTimes.forEach((lap, index) => {
+    const lapItem = document.createElement("li");
+    lapItem.textContent = `Lap ${index + 1}: ${lap}`;
+    lapList.appendChild(lapItem);
+  });
+};
